20bc26b2ce597f54f1b5692bd5f4f93024503869
sunchongsheng <sunchongsheng888@126.com>
2015-07-20 17:15:00 +0800

    测试github1
    这是我的第一个上传的gitHub文件

--- collectionView/ViewController.m
+++ collectionView/ViewController.m
@@ -17,7 +17,70 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //self.automaticallyAdjustsScrollViewInsets = YES;
+    
+    
+    UICollectionViewFlowLayout *flow = [[UICollectionViewFlowLayout alloc] init];
+    [flow setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+
+    flow.minimumLineSpacing = 0;
+    
+    //flow.minimumInteritemSpacing
+    
+    UICollectionView * collectionView = [[UICollectionView alloc]initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)collectionViewLayout:flow];
+    
+    collectionView.pagingEnabled = YES;
+    
+    collectionView.delegate = self;
+    
+    collectionView.dataSource = self;
+    
+    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellId"];
+    
+    NSLog(@"%f",self.view.frame.size.width);
+    
+    [self.view addSubview:collectionView];
+    
+    
 }
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 10;
+
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+
+    return 1;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+}
+
+
+-(UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section
+{
+    return UIEdgeInsetsMake(0, 0, 0, 0);
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cellId" forIndexPath:indexPath];
+    
+    cell.backgroundColor = [UIColor colorWithRed:(arc4random() % 100) / 100.0 green:(arc4random() % 100) / 100.0 blue:(arc4random() % 100) / 100.0 alpha:1];
+    
+    return cell;
+
+}
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
